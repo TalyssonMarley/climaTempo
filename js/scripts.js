30b1b126a43ea3f3368f476d1683c80d7067f52b
TalyssonMarley <talyssonmarley01@gmail.com>
2024-02-16 11:27:17 -0300

    functions

---
+++ js/scripts.js
@@ -0,0 +1,69 @@
+const cityInput = document.querySelector("#cityInput");
+const searchBtn = document.querySelector("#search");
+
+const cityName = document.querySelector("#cityName");
+const temp = document.querySelector("#temperature span");
+const description = document.querySelector("#description")
+const weather = document.querySelector("#weatherIcon");
+const humidity = document.querySelector(".humidity span");
+const wind = document.querySelector(".wind span");
+const flag = document.querySelector('#weatherData img');
+
+const weatherDataContainer = document.querySelector("#weatherData");
+const errorMensage = document.querySelector('#erroMensage');
+
+const getWeatherData = async (city) => {
+  hideInfo();
+
+  const  apiWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&lang=pt_br&units=metric&appid=cfceb3fd2346b68d8137fb36162c7367`;
+  const apiResponse = await fetch(apiWeatherURL);
+  const data = await apiResponse.json();
+  
+  if (apiResponse.status === 200) {
+    return data;
+  } else {
+    showError();
+  }
+}
+const showWeaterData = async (city) => {
+  const data = await getWeatherData(city);
+
+  cityInput.value = "";
+
+  cityName.innerText = data.name;
+  temp.innerText = parseInt(data.main.temp);
+  description.innerText = data.weather[0].description;
+  weather.setAttribute("src", `https://openweathermap.org/img/wn/${data.weather[0].icon}.png` )
+  humidity.innerText = `${data.main.humidity}%`;
+  wind.innerText = `${data.wind.speed}km/h`;
+  flag.setAttribute("src", `https://flagsapi.com/${data.sys.country}/flat/32.png`);
+
+  weatherDataContainer.style.display = 'block';
+
+  console.log(data);
+};
+
+const showError = () => {
+  errorMensage.style.display = 'block';
+}
+
+const hideInfo = () => {
+  weatherDataContainer.style.display = 'none';
+  errorMensage.style.display = 'none';
+}
+
+searchBtn.addEventListener('click', (event)=> {
+  event.preventDefault();
+
+  const city = cityInput.value;
+
+  showWeaterData(city);
+});
+
+cityInput.addEventListener('keyup', (event) => {
+  if(event.code === "Enter") {
+    const city = event.target.value;
+
+  showWeaterData(city);
+  }
+})
